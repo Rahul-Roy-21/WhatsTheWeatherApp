e9789f4130a0a3820f6c5717a60edbff776305c7
Rahul_Roy_21 <royrahul0921@gmail.com>
2021-10-27 02:12:27 +0530

    Modified JS of Progress

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const windSpeed = document.querySelector(".wind");
 const AQIElement = document.querySelector(".aqiElement");
 
 // Accuweather API key
-const key = "UiPSA87IdIYJmHapTXhtzDrc7UvPJDgg";
+const key = "uXM70077fLmjghIS4IlSBdCk3lm3Qxc3";
 
 // API Fetch Storage
 const weather = {};
@@ -41,48 +41,51 @@ var localTime = "";
 
 hideResults();
 
-submitButton.addEventListener("click", function(){
+submitButton.addEventListener("click", function () {
     var input = searchInput.value;
-    if(input == ""){
+    if (input == "") {
         displayNotificationBar("No City Entered !!", "danger");
         hideResults();
     }
-    else{
+    else {
         getWeather(input);
     }
 });
 
-function getWeather(input){
+function getWeather(input) {
     const locationApi = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${input}`;
 
     fetch(locationApi)
-        .then(function(response){
+        .then(function (response) {
             let data = response.json();
             return data;
         })
-        .then(function(data){
-            if(!data[0]){
+        .then(function (data) {
+            if (!data[0]) {
                 displayNotificationBar("No Such City Exists !!", "danger");
                 hideResults();
             }
-            else{
+            else {
                 weather.city = `${data[0]["EnglishName"]}, ${data[0]["Country"]["EnglishName"]}`;
                 getWeatherwithKey(data[0]["Key"]);
             }
+        })
+        .catch((error) => {
+            console.error('My Error:', error);
         });
-        
+
 }
 
-function getWeatherwithKey(locationKey){
+function getWeatherwithKey(locationKey) {
     const currentconditionsApi = `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${key}&details=true`;
 
     fetch(currentconditionsApi)
-        .then(function(response){
+        .then(function (response) {
             let data = response.json();
             return data;
         })
-        .then(function(data){
-    
+        .then(function (data) {
+
             localTime = data[0]["LocalObservationDateTime"];
             weather.weatherText = data[0]["WeatherText"];
 
@@ -99,16 +102,16 @@ function getWeatherwithKey(locationKey){
             weather.UVIndex = `${data[0]["UVIndex"]} (${data[0]["UVIndexText"]})`;
             displayWeather1();
         });
-    
+
     const next5days = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${key}&details=true`;
 
     fetch(next5days)
-        .then(function(response){
+        .then(function (response) {
             let datanext = response.json();
             return datanext;
         })
-        .then(function(datanext){
-            
+        .then(function (datanext) {
+
             weather.next5daysweather = datanext["DailyForecasts"][0]["Link"];
             weather.sunrise = datanext["DailyForecasts"][0]["Sun"]["Rise"];
             weather.sunset = datanext["DailyForecasts"][0]["Sun"]["Set"];
@@ -128,17 +131,17 @@ function getWeatherwithKey(locationKey){
             weather.maxTemp2 = Math.ceil(celsius(datanext["DailyForecasts"][2]["Temperature"]["Maximum"]["Value"]));
             weather.iconPhrase2 = datanext["DailyForecasts"][2]["Day"]["IconPhrase"];
             displayWeather2();
-            
+
         });
 
     const aqi = `http://dataservice.accuweather.com/indices/v1/daily/1day/${locationKey}/-10?apikey=${key}&details=true`;
 
     fetch(aqi)
-        .then(function(response){
+        .then(function (response) {
             let dataaqi = response.json();
             return dataaqi;
         })
-        .then(function(dataaqi){
+        .then(function (dataaqi) {
             weather.aqiCat = dataaqi[0]["Category"];
             weather.aqiText = dataaqi[0]["Text"];
             console.log("AQI:" + dataaqi[0]["Category"]);
@@ -146,10 +149,10 @@ function getWeatherwithKey(locationKey){
         });
 }
 
-function displayWeather1(){
+function displayWeather1() {
     hideNotificationBar();
     showResults();
-    
+
     console.log(hourmin(localTime));
     cityName.innerHTML = weather.city + `<br><h6><i class="fas fa-clock"></i> ${hourmin(localTime)}</h6>`;
     weatherIcon.src = `https://www.accuweather.com/images/weathericons/${weather.weatherIcon}.svg`;
@@ -161,11 +164,11 @@ function displayWeather1(){
     cloudsElement.innerHTML = `${weather.cloudcover}%`;
     pressure.innerHTML = `${weather.pressure}mbar`;
     windSpeed.innerHTML = `${weather.wind} km/hr`;
-    
+
     linkAccuWeather.href = weather.currentForecastLink;
 }
 
-function displayWeather2(){
+function displayWeather2() {
     sunRiseElement.innerHTML = `Sunrise ${hourmin(weather.sunrise)}`;
     sunSetElement.innerHTML = `Sunset ${hourmin(weather.sunset)}`;
     setProgress(localTime, weather.sunrise, weather.sunset);
@@ -184,56 +187,57 @@ function displayWeather2(){
     dayaftertomorrowTemp.innerHTML = `${weather.maxTemp2}&deg;/${weather.minTemp2}&deg`;
 }
 
-function displayWeather3(){
+function displayWeather3() {
     AQIElement.innerHTML = weather.aqiCat;
     AQIElement.setAttribute('title', weather.aqiText);
 }
 
 
-function hourmin(iso){
-    let hr = Number(iso.slice(11,13));
-    let min = iso.slice(14,16);
+function hourmin(iso) {
+    let hr = Number(iso.slice(11, 13));
+    let min = iso.slice(14, 16);
     let abbr = 'AM';
-    if(hr > 12){
-        hr = hr-12;
-        abbr= 'PM';
+    if (hr > 12) {
+        hr = hr - 12;
+        abbr = 'PM';
     }
-    if(hr < 10){
-        hr = "0"+hr;
+    if (hr < 10) {
+        hr = "0" + hr;
     }
-    return (hr+":"+ min + abbr);
+    return (hr + ":" + min + abbr);
 }
 
-function setProgress(cur, rise, set){
+function setProgress(cur, rise, set) {
     console.log("Current:" + cur);
     console.log("Rise:" + rise);
     console.log("set:" + set);
 
-    minutes_cur = Number(cur.slice(11,13))*60 + Number(cur.slice(14,16));
-    minutes_rise = Number(rise.slice(11,13))*60 + Number(rise.slice(14,16));
-    minutes_set = Number(set.slice(11,13))*60 + Number(set.slice(14,16));
+    minutes_cur = Number(cur.slice(11, 13)) * 60 + Number(cur.slice(14, 16));
+    minutes_rise = Number(rise.slice(11, 13)) * 60 + Number(rise.slice(14, 16));
+    minutes_set = Number(set.slice(11, 13)) * 60 + Number(set.slice(14, 16));
 
     let progress = 0;
-    if(minutes_cur < minutes_rise){
+    if (minutes_cur < minutes_rise) {
         progress = 0;
     }
-    else if(minutes_cur > minutes_set){
+    else if (minutes_cur > minutes_set) {
         progress = 100;
     }
-    else{
-        progress = Math.floor(100*(minutes_cur - minutes_rise)/(minutes_set - minutes_rise));
+    else {
+        progress = Math.floor(100 * (minutes_cur - minutes_rise) / (minutes_set - minutes_rise));
     }
+
     progressBar.setAttribute("aria-valuenow", progress.toString());
-    progressBar.style.width = progress.toString()+"%";
-    progressBar.innerHTML = progress.toString()+" %";
-    console.log("Progress: "+progress);
+    progressBar.style.width = progress.toString() + "%";
+    progressBar.innerHTML = progress.toString() + " %";
+    console.log("Progress: " + progress);
 }
 
-function After2Days(Time){
-    const d = new Date(Time.slice(0,4), Time.slice(5,7), Time.slice(8,9));
+function After2Days(Time) {
+    const d = new Date(Time.slice(0, 4), Time.slice(5, 7), Time.slice(8, 9));
     let day = "";
     //console.log("DAY: "+d.getDay())
-    switch(d.getDay()){
+    switch (d.getDay()) {
         case 3: day = "Sun"; break;
         case 4: day = "Mon"; break;
         case 5: day = "Tue"; break;
@@ -245,7 +249,7 @@ function After2Days(Time){
     return day;
 }
 
-function displayNotificationBar(message, alert){
+function displayNotificationBar(message, alert) {
     notificationBar.style.display = "block";
     notificationBar.innerHTML = message;
     notificationBar.className = '';
@@ -253,20 +257,20 @@ function displayNotificationBar(message, alert){
     notificationBar.classList.add(`alert-${alert}`);
 }
 
-function hideNotificationBar(){
+function hideNotificationBar() {
     notificationBar.style.display = "none";
 }
 
-function showResults(){
+function showResults() {
     Results.style.display = "block";
     Logo.style.display = "none";
 }
 
-function hideResults(){
+function hideResults() {
     Results.style.display = "none";
     Logo.style.display = "block";
 }
 
-function celsius(fah){
-    return ((fah-32)*(5/9));
+function celsius(fah) {
+    return ((fah - 32) * (5 / 9));
 }
